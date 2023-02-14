861b6bf96a503e28cfff3b253241f7e8bed5062e
iTSerhii85 <109918837+iTSerhii85@users.noreply.github.com>
2023-02-14 12:32:07 +0000

    Deploying to gh-pages from @ iTSerhii85/my_portfolio@3012ff3b18f9041ed2cc6f7bb40c6556e34e9a7b 🚀

---
+++ index.9e67ce5c.js
@@ -0,0 +1,2 @@
+const c=document.querySelector(".contact_btn"),e=document.querySelector(".contact_menu");c.addEventListener("click",(function(){console.log("click"),e.classList.add("hiden")}));
+//# sourceMappingURL=index.9e67ce5c.js.map
---
+++ index.ced1f4e2.js
@@ -0,0 +1,2 @@
+!function(){var c=document.querySelector(".contact_btn"),e=document.querySelector(".contact_menu");c.addEventListener("click",(function(){console.log("click"),e.classList.add("hiden")}))}();
+//# sourceMappingURL=index.ced1f4e2.js.map
